c289aa996d57a0c08932c68ea6b12904f786f42c
Santiago Sanchez <visanto.agencia@gmail.com>
2024-02-16 13:49:40 -0500

    commit 2

--- server/api.py
+++ server/api.py
@@ -3,10 +3,4 @@ from .routes import router as NoteRouter
 
 app = FastAPI()
 
-@app.get("/", tags=["Root"])
-async def read_root() -> dict:
-    return {
-        "message": "Welcome to my notes application, use the /docs route to proceed"
-    }
-
 app.include_router(NoteRouter, prefix="/note")
